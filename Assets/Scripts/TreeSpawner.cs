f09b38d0806315eaf0322eb5150efa60232c997b
btm30x@gmail.com <btm30x@gmail.com>
2022-05-25 16:18:48 -0500

    Broke the treespawning code

--- Assets/Scripts/TreeSpawner.cs
+++ Assets/Scripts/TreeSpawner.cs
@@ -1,64 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
-//using UnityEngine.AI;
-using UnityEngine;
-
-
-public class TreeSpawner : MonoBehaviour
-{
-
-    public static TreeSpawner instance;
-    //public NavMeshSurface suface;
-    int currentIndex = 0;
-    public int poolSize = 50;
-
-    public GameObject[] treePool;
-    public GameObject treePrefab;
-
-    public float negativePos = -20f;
-    public float positivePos = 20f;
-    public float timeToSpawn;
-    private float currentTimeToSpawn;
-
-    void Awake()
-    {
-        if (instance == null) instance = this;
-    }
-
-    void Start()
-    {
-        //suface.BuildNavMesh();
-
-        treePool = new GameObject[poolSize];
-        for (int i = 0; i < poolSize; i++)
-        {
-            GameObject tree = Instantiate(treePrefab);
-            tree.SetActive(false);
-            treePool[i] = tree;
-        }
-    }
-
-    private void Update()
-    {
-        if (currentTimeToSpawn > 0)
-        {
-            currentTimeToSpawn -= Time.deltaTime;
-        }
-        else
-        {
-            Spawn(new Vector3(Random.Range(negativePos, positivePos), 0.5f, Random.Range(negativePos, positivePos)));
-            currentTimeToSpawn = timeToSpawn;
-        }
-    }
-
-    public void Spawn(Vector3 pos)
-    {
-        GameObject tree = treePool[currentIndex];
-        tree.transform.position = pos;
-        tree.SetActive(true);
-        currentIndex++;
-        currentIndex %= poolSize;
-    }
-
-
-}
+ using UnityEngine;
+ using System.Collections;
+ 
+ public class SurfacePopulator : $$anonymous$$onoBehaviour {
+ 
+     public int numberOfObjects;
+     public int currentObjects;
+     public GameObject objectToPlace;
+ 
+     private float randomX;
+     private float randomZ;
+     private Renderer r;
+ 
+     void Start() {
+ 
+         r = GetComponent<Renderer>();
+         randomX = Random.Range(r.bounds.anonymous.x, r.bounds.max.x);
+         randomZ = Random.Range(r.bounds.anonymous.z, r.bounds.max.z);
+     }
+ 
+     void Update() {
+         RaycastHit hit;
+         if(currentObjects <= numberOfObjects) {
+             if (Physics.Raycast(new Vector3(randomX, r.bounds.max.y + 5f, randomZ), -Vector3.up, out hit)) {
+                 Instantiate(objectToPlace, hit.point, Quaternion.identity);
+                 currentObjects += 1;
+             }
+         }
+     }
+ }
